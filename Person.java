10c36b622033365f22d43ef8b708ba73ba6412fd
oglee <ecogle@gmail.com>
2017-07-10 06:44:54 -0500

    Versioned now

---
+++ Person.java
@@ -0,0 +1,35 @@
+public class Person{
+	private String name;
+	private int number;
+	
+	//setters
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	//getters
+	public void setNumber(int number){
+		this.number = number;
+	}
+	
+	public int getNumber(){
+		
+		return this.number;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public Person(String name, int number){
+		
+		this.name = name;
+		this.number = number;
+	}
+	
+	@Override
+	public String toString(){
+		return "My name is " + this.name + " and my number is " + this.number;
+	}
+	
+}

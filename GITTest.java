27e0f6d624f0ec47f2cb35e6a9dc85d4e15ce5dc
oglee <ecogle@gmail.com>
2017-07-10 06:33:56 -0500

    This is from John Doe

--- GITTest.java
+++ GITTest.java
@@ -3,5 +3,8 @@ public class GITTest{
 	public static void main(String[] args){
 		//This code was commited by Chad Ogle
 		System.out.println("Hello from Chad");
+		
+		//This is a commit by John Doe
+		System.out.println("This is from John Doe");
 	}
 }
